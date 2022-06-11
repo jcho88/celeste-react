892b1f2e7b84c4152b6c403c1b50a9a29f4cb18d
Justin Cho <justinmyungcho@gmail.com>
2022-06-11 13:25:44 -0700

    add place detail navigation

--- screens/MainMapScreen.tsx
+++ screens/MainMapScreen.tsx
@@ -1,58 +1,67 @@
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, Pressable } from 'react-native';
 import MapView from 'react-native-maps';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import { useState } from 'react';
-import placesData from '../assets/resources/places.json'
+import placesData from '../assets/resources/places.json';
+import PlaceDetail from './PlaceDetail';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  ...StyleSheet.absoluteFillObject,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+	container: {
+		flex: 1,
+		...StyleSheet.absoluteFillObject,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: '80%',
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
 });
 
 export default function MainMapScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+	const [places, setPlaces] = useState(placesData);
+	// const openDetail = (place: Place) => {
+	// 	return <PlaceDetail place={place} />;
+	// };
 
-  const [places, setPlaces] = useState(placesData);
-
-  return (
-    <>
-    <MapView 
-    style={styles.map}
-    initialRegion={{
-      latitude: 40.75251018277572, 
-      longitude: -73.97984077693457,
-      latitudeDelta: 0.3, 
-      longitudeDelta: 0.3
-    }}
-    ></MapView>
-      <ScrollView>
-        {places.map(place => 
-          <Text style={styles.title}>{place.name}</Text>
-        )}
-      </ScrollView>
-      {/* <View style={styles.container}>
+	return (
+		<>
+			<MapView
+				style={styles.map}
+				initialRegion={{
+					latitude: 40.75251018277572,
+					longitude: -73.97984077693457,
+					latitudeDelta: 0.3,
+					longitudeDelta: 0.3,
+				}}
+			></MapView>
+			<ScrollView>
+				{places.map((place) => (
+					<Pressable
+						onPress={() => {
+							navigation.navigate('PlaceDetail', place);
+						}}
+					>
+						<Text style={styles.title}>{place.name}</Text>
+					</Pressable>
+				))}
+			</ScrollView>
+			{/* <View style={styles.container}>
         <Text style={styles.title}>Tab One</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         <EditScreenInfo path="/screens/MainMapScreen.tsx" />
       </View> */}
-    </>
-  );
+		</>
+	);
 }
--- screens/PlaceDetail.tsx
+++ screens/PlaceDetail.tsx
@@ -2,6 +2,8 @@ import { StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+import { Place } from '../constants/Default';
 
 const styles = StyleSheet.create({
 	container: {
@@ -25,11 +27,16 @@ const styles = StyleSheet.create({
 	},
 });
 
-export default function PlaceDetail() {
+export default function PlaceDetail({ navigation, route }: RootTabScreenProps<'TabOne'>) {
+	const place = route.params ? route.params : Place;
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Tab Two</Text>
-			<Image style={styles.circle} source={require('../assets/images/turtlerock@2x.jpg')} />
+			<Text style={styles.title}>Details</Text>
+			<Text style={styles.title}>{place.name}</Text>
+			<Text style={styles.title}>{place.description}</Text>
+			<Text style={styles.title}>{place.category}</Text>
+			<Text style={styles.title}>{place.date}</Text>
+			<Image style={styles.circle} source={{ uri: 'https://reactjs.org/logo-og.png' }} />
 			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 			<EditScreenInfo path="/screens/TabTwoScreen.tsx" />
 		</View>
